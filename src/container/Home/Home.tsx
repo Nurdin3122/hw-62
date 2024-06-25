e8a9e7430e43430572f715f3dfdf42c7f782bbdb
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-06-25 18:52:29 +0600

    добавил еще больще информации

--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -26,6 +26,65 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <div className="container-2">
+                <h3>Услуги</h3>
+                <div className="block-cards">
+                    <div className="cards-services">
+                        <div className="info-cards-services">
+                            <p className="text-cards-services">Приемка квартиры без отделки</p>
+                            <span className="price-cards-services">70 руб/м2</span>
+                        </div>
+                        <div className="img-cards-services">
+                            <img src="/img/cards-services1.png" alt="#"/>
+                        </div>
+                    </div>
+                    <div className="cards-services">
+                        <div className="info-cards-services">
+                            <p className="text-cards-services">Приемка квартиры без отделки</p>
+                            <span className="price-cards-services">70 руб/м2</span>
+                        </div>
+                        <div className="img-cards-services">
+                            <img src="/img/cards-services2.png" alt="#"/>
+                        </div>
+                    </div>
+                    <div className="cards-services">
+                        <div className="info-cards-services">
+                            <p className="text-cards-services part-2">Приемка квартиры во вторичном жилье</p>
+                            <span className="price-cards-services">70 руб/м2</span>
+                        </div>
+                        <div className="img-cards-services">
+                            <img src="/img/cards-services3.png" alt="#"/>
+                        </div>
+                    </div>
+                    <div className="cards-services">
+                        <div className="info-cards-services">
+                            <p className="text-cards-services part-3">Контрольный замер площади</p>
+                            <span className="price-cards-services">10 руб/м2</span>
+                        </div>
+                        <div className="img-cards-services">
+                            <img src="/img/cards-services4.png" alt="#"/>
+                        </div>
+                    </div>
+                    <div className="cards-services">
+                        <div className="info-cards-services">
+                            <p className="text-cards-services">Составление плана квартиры в электронном виде</p>
+                            <span className="price-cards-services">30 руб/м2</span>
+                        </div>
+                        <div className="img-cards-services">
+                            <img src="/img/cards-services5.png" alt="#"/>
+                        </div>
+                    </div>
+                    <div className="cards-services">
+                        <div className="info-cards-services">
+                            <p className="text-cards-services">Заключение кадастрового инженера</p>
+                            <span className="price-cards-services">100 руб/м2</span>
+                        </div>
+                        <div className="img-cards-services">
+                            <img src="/img/cards-services5.png" alt="#"/>
+                        </div>
+                        </div>
+                </div>
+            </div>
         </>
     );
 };
